020abf9d427b934073ea3db6c302fdf7a6dfb293
skharat8 <5607079+skharat8@users.noreply.github.com>
2025-02-05 19:44:25 -0800

    Implement game logic to check & validate guesses

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,15 +1,8 @@
 import { useState } from "react";
 
-import { sample } from "../../utils";
-import { WORDS } from "../../data";
 import GuessInput from "../GuessInput/GuessInput";
 import GuessResults from "../GuessResults/GuessResults";
 import { NUM_OF_GUESSES_ALLOWED } from "../../constants.js";
-
-// Pick a random word on every pageload.
-const answer = sample(WORDS);
-// To make debugging easier, we'll log the solution in the console.
-console.info({ answer });
 
 function Game() {
   const [guessResults, setGuessResults] = useState([]);
--- src/components/Guess/Guess.jsx
+++ src/components/Guess/Guess.jsx
@@ -1,15 +1,30 @@
 import React from "react";
 
+import { sample } from "../../utils";
+import { WORDS } from "../../data";
 import { range } from "../../utils.js";
+import { checkGuess } from "../../game-helpers.js";
+
+// Pick a random word on every pageload.
+const answer = sample(WORDS);
+// To make debugging easier, we'll log the solution in the console.
+console.info({ answer });
 
 function Guess({ guess }) {
+  const results = checkGuess(guess, answer);
+
   return (
     <p className="guess">
-      {range(5).map((num) => (
-        <span key={num} className="cell">
-          {guess ? guess[num] : undefined}
-        </span>
-      ))}
+      {range(5).map((num) => {
+        return (
+          <span
+            key={num}
+            className={`cell ${guess ? results[num].status : ""}`}
+          >
+            {guess ? guess[num] : undefined}
+          </span>
+        );
+      })}
     </p>
   );
 }
